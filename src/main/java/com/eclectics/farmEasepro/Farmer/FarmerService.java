2468156df26a10786b6cf1426a2653a5a834b729
steve2030 <stevemighty2030@gmail.com>
2023-11-06 03:56:04 +0300

    modified the Service layer

--- src/main/java/com/eclectics/farmEasepro/Farmer/FarmerService.java
+++ src/main/java/com/eclectics/farmEasepro/Farmer/FarmerService.java
@@ -1,4 +1,11 @@
 package com.eclectics.farmEasepro.Farmer;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class FarmerService {
+    @Autowired
+    private FarmerRepository farmerRepository;
+
 }
