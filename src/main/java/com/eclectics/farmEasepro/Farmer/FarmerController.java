9bf485251c35d728f6e5ff71cc1c6cf3f418b42e
steve2030 <stevemighty2030@gmail.com>
2023-11-06 03:53:46 +0300

    initialized a Post Request

--- src/main/java/com/eclectics/farmEasepro/Farmer/FarmerController.java
+++ src/main/java/com/eclectics/farmEasepro/Farmer/FarmerController.java
@@ -1,4 +1,15 @@
 package com.eclectics.farmEasepro.Farmer;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class FarmerController {
+
+    @GetMapping(path = "api/v1/farmers")
+    public List<Farmer>getFarmers(){
+        return farmerService.getFarmers();
+    }
 }
