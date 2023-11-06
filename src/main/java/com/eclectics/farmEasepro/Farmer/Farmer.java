8e28dded9231fd818d970fa91c8d1857959a44ab
steve2030 <stevemighty2030@gmail.com>
2023-11-06 03:43:17 +0300

    modified the entity class

--- src/main/java/com/eclectics/farmEasepro/Farmer/Farmer.java
+++ src/main/java/com/eclectics/farmEasepro/Farmer/Farmer.java
@@ -1,7 +1,104 @@
 package com.eclectics.farmEasepro.Farmer;
 
-import jakarta.persistence.Entity;
-
 
 public class Farmer {
+
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+
+
+    private String email;
+
+    private int  phoneNumber;
+
+
+//    constructor class
+
+    //first consructor
+    public Farmer(Long id) {
+        this.id = id;
+    }
+
+    //second constructor
+    public Farmer(Long id,
+                  String firstName,
+                  String lastName,
+                  String email,
+                  int phoneNumber
+    ) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+//third constructor
+
+//    public Farmer(String firstName, String lastName, String email, int phoneNumber) {
+//        this.firstName = firstName;
+//        this.lastName = lastName;
+//        this.email = email;
+//        this.phoneNumber = phoneNumber;
+//    }
+
+//    getters and setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+//    to_string
+
+    @Override
+    public String toString() {
+        return "Farmer{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", phoneNumber=" + phoneNumber +
+                '}';
+    }
 }
+
+
