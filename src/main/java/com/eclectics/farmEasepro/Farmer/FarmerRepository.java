fc4b3a3addd3a6e9389d0bd366671f89def739a4
steve2030 <stevemighty2030@gmail.com>
2023-11-06 12:46:15 +0300

    mde some modifications

--- src/main/java/com/eclectics/farmEasepro/Farmer/FarmerRepository.java
+++ src/main/java/com/eclectics/farmEasepro/Farmer/FarmerRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class FarmerRepository extends JpaRepository<Long, Id> {
+public Interface FarmerRepository extends JpaRepository<Long, Id> {
 }
