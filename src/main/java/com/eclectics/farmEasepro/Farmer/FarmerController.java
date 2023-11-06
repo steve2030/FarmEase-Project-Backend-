aa078d326778e8befbb2e444d621803d21cda0a8
steve2030 <stevemighty2030@gmail.com>
2023-11-06 03:57:33 +0300

    autowired the service layer

--- src/main/java/com/eclectics/farmEasepro/Farmer/FarmerController.java
+++ src/main/java/com/eclectics/farmEasepro/Farmer/FarmerController.java
@@ -1,5 +1,6 @@
 package com.eclectics.farmEasepro.Farmer;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +8,8 @@ import java.util.List;
 
 @RestController
 public class FarmerController {
-
+@Autowired
+private FarmerService farmerService;
     @GetMapping(path = "api/v1/farmers")
     public List<Farmer>getFarmers(){
         return farmerService.getFarmers();
