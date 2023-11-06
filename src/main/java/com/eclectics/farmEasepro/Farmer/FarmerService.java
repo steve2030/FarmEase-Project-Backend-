a2edf313518a2539cf432085c9d3a4f417ff9576
steve2030 <stevemighty2030@gmail.com>
2023-11-06 04:00:21 +0300

    added a post method to fetch all the farmers

--- src/main/java/com/eclectics/farmEasepro/Farmer/FarmerService.java
+++ src/main/java/com/eclectics/farmEasepro/Farmer/FarmerService.java
@@ -3,9 +3,17 @@ package com.eclectics.farmEasepro.Farmer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class FarmerService {
     @Autowired
     private FarmerRepository farmerRepository;
+
+
+    // Post Method
+    public List<Farmer>getFarmers(){
+        return farmerRepository.findAll();
+    }
 
 }
