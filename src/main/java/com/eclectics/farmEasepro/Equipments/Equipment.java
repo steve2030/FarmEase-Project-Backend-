e3c7a0640b48c6433a83978d56a5db3109bcd462
steve2030 <stevemighty2030@gmail.com>
2023-11-07 21:40:31 +0300

    modified the entity class

--- src/main/java/com/eclectics/farmEasepro/Equipments/Equipment.java
+++ src/main/java/com/eclectics/farmEasepro/Equipments/Equipment.java
@@ -1,11 +1,15 @@
 package com.eclectics.farmEasepro.Equipments;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class Equipment {
+
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String type;
