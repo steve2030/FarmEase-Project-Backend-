4e805e84ea6230e08f2ca63c3434c3ab2211bd49
steve2030 <stevemighty2030@gmail.com>
2023-11-07 03:47:20 +0300

    modified the getters and setters class

--- src/main/java/com/eclectics/farmEasepro/Farmer/Farmer.java
+++ src/main/java/com/eclectics/farmEasepro/Farmer/Farmer.java
@@ -42,7 +42,45 @@ public class Farmer {
 
 //    getters and setters
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 
 
 //    to_string
