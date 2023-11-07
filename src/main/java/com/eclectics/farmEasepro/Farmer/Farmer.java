dd252ff17b5026d10b7a316d3feb2c1d65379211
steve2030 <stevemighty2030@gmail.com>
2023-11-07 04:12:42 +0300

    modified the farmer class

--- src/main/java/com/eclectics/farmEasepro/Farmer/Farmer.java
+++ src/main/java/com/eclectics/farmEasepro/Farmer/Farmer.java
@@ -1,4 +1,21 @@
 package com.eclectics.farmEasepro.Farmer;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Farmer {
+    @Id
+//
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String secondName;
+    private String email;
+    private String phoneNo;
+    private String County;
+    private String localArea;
+
 }
