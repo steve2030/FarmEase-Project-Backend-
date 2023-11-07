a97eb3f259add230a7969894b00cd452391cb56c
steve2030 <stevemighty2030@gmail.com>
2023-11-07 07:44:45 +0300

    modified the entity class

--- src/main/java/com/eclectics/farmEasepro/Equipments/Equipment.java
+++ src/main/java/com/eclectics/farmEasepro/Equipments/Equipment.java
@@ -1,4 +1,93 @@
 package com.eclectics.farmEasepro.Equipments;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Equipment {
+    @Id
+    private Long id;
+
+    private String type;
+    private String model;
+    private Boolean available;
+    private int rate;
+
+
+//    Default constructor
+
+    public Equipment() {
+    }
+
+//    The main constructor
+
+    public Equipment(Long id,
+                     String type,
+                     String model,
+                     Boolean available,
+                     int rate) {
+        this.id = id;
+        this.type = type;
+        this.model = model;
+        this.available = available;
+        this.rate = rate;
+    }
+
+//    Getters and setters
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public Boolean getAvailable() {
+        return available;
+    }
+
+    public void setAvailable(Boolean available) {
+        this.available = available;
+    }
+
+    public int getRate() {
+        return rate;
+    }
+
+    public void setRate(int rate) {
+        this.rate = rate;
+    }
+
+
+//    to string
+
+    @Override
+    public String toString() {
+        return "Equipment{" +
+                "id=" + id +
+                ", type='" + type + '\'' +
+                ", model='" + model + '\'' +
+                ", available=" + available +
+                ", rate=" + rate +
+                '}';
+    }
 }
+
