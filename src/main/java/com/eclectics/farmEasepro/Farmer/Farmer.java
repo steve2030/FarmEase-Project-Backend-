4c2692247dce1e92ce4d75d64ef11536a702a3b7
steve2030 <stevemighty2030@gmail.com>
2023-11-07 04:15:02 +0300

    Initialized the constructors for the entity class

--- src/main/java/com/eclectics/farmEasepro/Farmer/Farmer.java
+++ src/main/java/com/eclectics/farmEasepro/Farmer/Farmer.java
@@ -18,4 +18,29 @@ public class Farmer {
     private String County;
     private String localArea;
 
+//    default constructor
+
+    public Farmer(Long id) {
+        this.id = id;
+    }
+
+//    the other constructor
+
+    public Farmer(Long id,
+                  String firstName,
+                  String secondName,
+                  String email,
+                  String phoneNo,
+                  String county,
+                  String localArea) {
+
+        this.id = id;
+        this.firstName = firstName;
+        this.secondName = secondName;
+        this.email = email;
+        this.phoneNo = phoneNo;
+        County = county;
+        this.localArea = localArea;
+    }
+
 }
