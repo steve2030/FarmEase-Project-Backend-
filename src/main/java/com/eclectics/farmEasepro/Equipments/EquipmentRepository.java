82e8facfae77e3c62ca0edee9f748f4ddc825977
steve2030 <stevemighty2030@gmail.com>
2023-11-07 07:51:19 +0300

    created a jpa repository

--- src/main/java/com/eclectics/farmEasepro/Equipments/EquipmentRepository.java
+++ src/main/java/com/eclectics/farmEasepro/Equipments/EquipmentRepository.java
@@ -1,4 +1,8 @@
 package com.eclectics.farmEasepro.Equipments;
 
-public interface EquipmentRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EquipmentRepository extends JpaRepository<Equipment Id>{
 }
