56f525bd7092af785cbf7f1b0a71c1ffd5688d7b
steve2030 <stevemighty2030@gmail.com>
2023-11-07 08:48:27 +0300

    modified

--- src/main/java/com/eclectics/farmEasepro/Equipments/EquipmentController.java
+++ src/main/java/com/eclectics/farmEasepro/Equipments/EquipmentController.java
@@ -1,8 +1,26 @@
 package com.eclectics.farmEasepro.Equipments;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class EquipmentController {
+    @Autowired
+    private EquipmentService equipmentService;
+    @GetMapping(path="/api/v1/equipments")
+    public List<Equipment>getEquipments(){
+       return  equipmentService.getEquipments();
+    }
+
+
+
+
+
+
+
+
 
 }
--- src/main/java/com/eclectics/farmEasepro/Equipments/EquipmentService.java
+++ src/main/java/com/eclectics/farmEasepro/Equipments/EquipmentService.java
@@ -1,11 +1,13 @@
 package com.eclectics.farmEasepro.Equipments;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class EquipmentService {
+    @Autowired
 
     private EquipmentRepository equipmentRepository;
 
