099e9b7b5d7a642a40a3f1e2e8ff3c4bfcf27106
steve2030 <stevemighty2030@gmail.com>
2023-11-08 04:25:26 +0300

    moified the entity class

--- src/main/java/com/eclectics/farmEasepro/Equipments/Equipment.java
+++ src/main/java/com/eclectics/farmEasepro/Equipments/Equipment.java
@@ -1,6 +1,7 @@
 package com.eclectics.farmEasepro.Equipments;
 
 import jakarta.persistence.Entity;
+
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
