4c2edeaa8070885665d640c0a34299e5f2a32a31
stephen munyao <stevemighty2030@gmail.com>
2023-11-24 18:06:21 +0300

    modified the entity class

--- src/main/java/com/eclectics/farmEasepro/Equipments/Equipment.java
+++ src/main/java/com/eclectics/farmEasepro/Equipments/Equipment.java
@@ -1,12 +1,12 @@
 package com.eclectics.farmEasepro.Equipments;
 
-import jakarta.persistence.Entity;
+import com.eclectics.farmEasepro.Farmer.Farmer;
+import jakarta.persistence.*;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 
 @Entity
+
+
 public class Equipment {
 
     @Id
@@ -19,6 +19,67 @@ public class Equipment {
     private int rate;
 
 
+    public Equipment() {
 
+    }
+
+    public Equipment(Long id, String type, String model, Boolean available, int rate) {
+        this.id = id;
+        this.type = type;
+        this.model = model;
+        this.available = available;
+        this.rate = rate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public Boolean getAvailable() {
+        return available;
+    }
+
+    public void setAvailable(Boolean available) {
+        this.available = available;
+    }
+
+    public int getRate() {
+        return rate;
+    }
+
+    public void setRate(int rate) {
+        this.rate = rate;
+    }
+
+    @Override
+    public String toString() {
+        return "Equipment{" +
+                "id=" + id +
+                ", type='" + type + '\'' +
+                ", model='" + model + '\'' +
+                ", available=" + available +
+                ", rate=" + rate +
+                '}';
+    }
 }
 
