2573ffb7fe064b85d834dad9c0e8dfb79e7137d9
stephen munyao <stevemighty2030@gmail.com>
2023-11-24 15:29:13 +0300

    modified the farmer entity class and POM.XML

--- src/main/java/com/eclectics/farmEasepro/Farmer/Farmer.java
+++ src/main/java/com/eclectics/farmEasepro/Farmer/Farmer.java
@@ -3,9 +3,10 @@ package com.eclectics.farmEasepro.Farmer;
 import jakarta.persistence.*;
 
 @Entity
+
 public class Farmer {
     @Id
-//
+
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
@@ -22,99 +23,8 @@ public class Farmer {
     @Column
     private String localArea;
 
-//    default constructor
-
-    public Farmer() {
-
-    }
-
-//    the other constructor
-
-    public Farmer(Long id,
-                  String firstName,
-                  String secondName,
-                  String email,
-                  String phoneNo,
-                  String county,
-                  String localArea) {
-
-        this.id = id;
-        this.firstName = firstName;
-        this.secondName = secondName;
-        this.email = email;
-        this.phoneNo = phoneNo;
-        County = county;
-        this.localArea = localArea;
-    }
-// getters and setters
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getSecondName() {
-        return secondName;
-    }
-
-    public void setSecondName(String secondName) {
-        this.secondName = secondName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhoneNo() {
-        return phoneNo;
-    }
-
-    public void setPhoneNo(String phoneNo) {
-        this.phoneNo = phoneNo;
-    }
-
-    public String getCounty() {
-        return County;
-    }
-
-    public void setCounty(String county) {
-        County = county;
-    }
-
-    public String getLocalArea() {
-        return localArea;
-    }
-
-    public void setLocalArea(String localArea) {
-        this.localArea = localArea;
-    }
 
 
-    @Override
-    public String toString() {
-        return "Farmer{" +
-                "id=" + id +
-                ", firstName='" + firstName + '\'' +
-                ", secondName='" + secondName + '\'' +
-                ", email='" + email + '\'' +
-                ", phoneNo='" + phoneNo + '\'' +
-                ", County='" + County + '\'' +
-                ", localArea='" + localArea + '\'' +
-                '}';
-    }
+
+
 }
