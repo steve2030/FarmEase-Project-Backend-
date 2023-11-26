1792cca7836a5111c76b31b55a0cdc4db5eef3f9
stephen munyao <stevemighty2030@gmail.com>
2023-11-26 16:06:19 +0300

    modified the project

--- src/main/java/com/eclectics/farmEasepro/Farmer/Farmer.java
+++ src/main/java/com/eclectics/farmEasepro/Farmer/Farmer.java
@@ -2,9 +2,15 @@ package com.eclectics.farmEasepro.Farmer;
 
 import com.eclectics.farmEasepro.Equipments.Equipment;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 
 
 
@@ -29,87 +35,5 @@ public class Farmer {
     @OneToOne
     private Equipment equipment;
 
-    public Farmer() {
 
-    }
-
-    public Farmer(Long id, String firstName, String secondName, String email, String phoneNo, String county, String localArea) {
-        this.id = id;
-        this.firstName = firstName;
-        this.secondName = secondName;
-        this.email = email;
-        this.phoneNo = phoneNo;
-        County = county;
-        this.localArea = localArea;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getSecondName() {
-        return secondName;
-    }
-
-    public void setSecondName(String secondName) {
-        this.secondName = secondName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhoneNo() {
-        return phoneNo;
-    }
-
-    public void setPhoneNo(String phoneNo) {
-        this.phoneNo = phoneNo;
-    }
-
-    public String getCounty() {
-        return County;
-    }
-
-    public void setCounty(String county) {
-        County = county;
-    }
-
-    public String getLocalArea() {
-        return localArea;
-    }
-
-    public void setLocalArea(String localArea) {
-        this.localArea = localArea;
-    }
-
-
-    @Override
-    public String toString() {
-        return "Farmer{" +
-                "id=" + id +
-                ", firstName='" + firstName + '\'' +
-                ", secondName='" + secondName + '\'' +
-                ", email='" + email + '\'' +
-                ", phoneNo='" + phoneNo + '\'' +
-                ", County='" + County + '\'' +
-                ", localArea='" + localArea + '\'' +
-                '}';
-    }
 }
