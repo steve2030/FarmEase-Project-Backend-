29651137ce81e2b9d778f40a608cdb4a777749b8
stephen munyao <stevemighty2030@gmail.com>
2023-11-26 14:45:22 +0300

    Used lombok for entity class

--- src/main/java/com/eclectics/farmEasepro/Equipments/Equipment.java
+++ src/main/java/com/eclectics/farmEasepro/Equipments/Equipment.java
@@ -2,11 +2,18 @@ package com.eclectics.farmEasepro.Equipments;
 
 import com.eclectics.farmEasepro.Farmer.Farmer;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 
 @Entity
 
-
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
 public class Equipment {
 
     @Id
@@ -19,67 +26,6 @@ public class Equipment {
     private int rate;
 
 
-    public Equipment() {
 
-    }
-
-    public Equipment(Long id, String type, String model, Boolean available, int rate) {
-        this.id = id;
-        this.type = type;
-        this.model = model;
-        this.available = available;
-        this.rate = rate;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public String getModel() {
-        return model;
-    }
-
-    public void setModel(String model) {
-        this.model = model;
-    }
-
-    public Boolean getAvailable() {
-        return available;
-    }
-
-    public void setAvailable(Boolean available) {
-        this.available = available;
-    }
-
-    public int getRate() {
-        return rate;
-    }
-
-    public void setRate(int rate) {
-        this.rate = rate;
-    }
-
-    @Override
-    public String toString() {
-        return "Equipment{" +
-                "id=" + id +
-                ", type='" + type + '\'' +
-                ", model='" + model + '\'' +
-                ", available=" + available +
-                ", rate=" + rate +
-                '}';
-    }
 }
 
